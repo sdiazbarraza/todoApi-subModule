a4e8c6062423adbaaf368e1a2b3474dce4720512
simp <simpdev07@gmail.com>
2017-10-16 22:41:03 -0300

    Setup app for hero and for MLab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise=global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://sdb_dev:Carmen123@ds121535.mlab.com:21535/todo_api' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
