aa657caf8daa41a3fadee04433c8dd734c0d4ec4
simp <simpdev7@gmail.com>
2017-10-25 21:16:10 -0300

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -93,10 +93,20 @@ app.post('/users',(req,res)=>{
 		res.status(400).send(e);
 	});
 });
-
 app.get('/users/me',authenticate,(req,res)=>{
 	var token = req.header('x-auth');	
 	res.send(req.user);
+});
+app.post('/users/login',(req,res)=>{
+	var body = _.pick(req.body,['email','password']);
+
+	User.findByCredentials(body.email,body.password).then((user)=>{
+		return user.generateAuthToken().then((token)=>{
+			res.header('x-auth',token).send(user);
+		});
+	}).catch((e)=>{
+		res.status(400).send();
+	});
 });
 app.listen(port,()=>{
 	console.log(`Started on port ${port}`);
